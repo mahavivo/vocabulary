1c88c499b6eb4dff400223129b3c4fa57d7c75d1
vsha <visualdba@hotmail.com>
2017-09-10 09:53:21 +0800

    划分目录结构
    1. 参考https://github.com/kennethreitz/samplemod 对目录重新划分
    2. 增加AntBNC_lemmas_ver_001.txt 和 e_lemma.txt文件

---
+++ src/log.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import logging
+
+def log(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    fh = logging.FileHandler('hard_word_trans.log')
+    fh.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
